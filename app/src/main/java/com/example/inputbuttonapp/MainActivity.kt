cc1332cb406f67eb32f7219bf03801c7dba161d7
IgnacioLazZam <ignacio.laz.zam@gmail.com>
2024-09-24 12:25:04 +0200

    Input,button and snackbar

---
+++ app/src/main/java/com/example/inputbuttonapp/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.inputbuttonapp
+
+import android.os.Bundle
+import android.renderscript.ScriptGroup.Input
+import android.service.autofill.Sanitizer
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val button = findViewById<Button>(R.id.greetBtn);
+        button.setOnClickListener {
+            val input = findViewById<TextInputEditText>(R.id.textField).text.toString();
+
+            Snackbar.make(button, getString(R.string.Mensaje, input),Snackbar.LENGTH_SHORT).show()
+        }
+    }
+}
